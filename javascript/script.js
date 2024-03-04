aefac7e619a4ecc606e9f61813cf85895d918051
Vijay6923 <kumarviju2468@gmail.com>
2024-03-04 13:54:02 +0530

    html and css

---
+++ javascript/script.js
@@ -0,0 +1,59 @@
+// let a
+// console.log(a);
+
+// let ioi=[1,2,"ioistudent",true];
+// console.log(ioi);
+// let user1={
+//     name:'nishchay',
+//     age:18,
+// }
+// console.log(user1.name)
+// if else,switch,ternary
+// let bank=10;
+// if(bank>=10){
+//     console.log("ALLOWED CAR");
+// }else{
+//     console.log("NOT ELIGIBLE");
+// }
+// switch(bank){
+//     case 10:
+//         console.log("ALLOWED");
+//         break;
+//     case 9:
+//         console.log("not allowed");
+//         break;
+// }
+// let rain=true;
+// rain?console.log("it is raining") :console.log("not raining");
+// let n=10
+// let i;
+// for(int =1;n<=10;i++){
+//     console.log("pwioi")
+// }
+// function prakhar() {
+//     let mob=1234567890;
+//     console.log("prakhar sahu",mob);
+// }
+// prakhar();
+// javascript me koun se tarike se function call hoti hai by reference or by value?
+// function ioi (prm1,prm2){
+//     let cal=prm2+prm1*0.8;
+//     return cal;
+
+// }
+// let printake =ioi(80,90);
+// console.log(printake)
+// function pwioi(satya){
+//     let sum=0;
+
+//     for(let i=0;i<satya.length;i++){
+//         sum+=satya[i];
+//     }
+//     return sum;
+
+
+// }
+// let data =[1,2,3,4,5,6,7,8,9,10]
+// console.log(pwioi(data))
+
+
