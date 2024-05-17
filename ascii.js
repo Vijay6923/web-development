90b975e94e2bff2b23f663182c3fa4e9b9cb20b0
Vijay6923 <kumarviju2468@gmail.com>
2024-05-17 11:12:00 +0530

    revision

---
+++ ascii.js
@@ -0,0 +1,4 @@
+function AsciiCharacter(number) {
+    return String.fromCharCode(number);
+}
+console.log(AsciiCharacter(65))
--- script.js
+++ script.js
@@ -36,8 +36,70 @@
 // num=29?console.log("raman"):console.log("rohit")
 // function
 // most important is Arrow (=>) function
+// with parameter
+// lexical environment
 // function  ioi(){
     // console.log ("hello world");
 
 // }
 // ioi ()
+// function ioi(){
+//     let nam1="pw";
+//     let nam2="ioi";
+//     let nam3=nam1+nam2;
+//     return nam3;
+// }
+// let nam4=ioi();
+// console.log(nam4);
+// function ioi(nam1,nam2){
+    
+//     let nam3=nam1+nam2;
+//     return nam3;
+// }
+// let nam4=ioi("pw","ioi","xyz");
+// console.log(nam4);
+// function with n no. of parameter
+// function sum(num){
+//     sum=0;
+//     for (let i=0;i<5;i++){
+//         sum += num[i];
+//     }
+//     return sum;
+// }
+// num=[1,2,3,4,5];
+// const total=sum(num);
+// console.log(total);
+// arguments
+// self invoking function is also called as IILE.
+// HOF=>higher order function
+// function sum(){
+//     sum=0;
+//     for (let i=0;i<arguments.length;i++){
+//         sum += arguments[i];
+//     }
+//     return sum;
+// }
+
+// const total=sum(1,2,3,4,5);
+// console.log(total);
+// let name =() =>{
+//     console.log("VIJAY");
+// };
+// name();
+let sum=(a,b)=>{
+    console.log(a+b);
+    
+}
+sum(4,5);
+
+
+
+
+
+
+
+
+
+
+
+
