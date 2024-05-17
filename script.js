d5365971b087cd8a3c5c19af81996d0af45b7f1c
Vijay6923 <kumarviju2468@gmail.com>
2024-05-17 09:29:56 +0530

    revision

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-console.log(parv);
-sum ();
-let parv = "ioi";
-function sum(){
-    console.log("hello")
+// var name1="IOI"
+// let name2="IOI2"
+// const name3 ="IOI3"
+// console.log(name3)
+// datatypes
+// number => int ,float,double
+// string => char,comb,char
+// boolean =>true or false
+// undefined =>when variable is declared but the value is not assigned
+// null =>when there is no value
+// let loan_num=null;
+// loan_num=3;
+// Array=>[],key is index
+// object=>{},key is of your choice
+// 1.everything in javascript is object.2=>value is always travel from left to right 
+// operators=> assignment,arthmetic=(+,-,*,/,%),logical=(AND,OR),relational=> >,<,Bitwise on hold
 
-}
+// conditions:-
+//  if(){
+    // block true
+//  } else if(){
+    // 
+// }else{
 
+// }
+
+// switch:-
+// switch(){
+    // case1:{
+        // break;
+    // }
+// }
+// ternary op:
+// cond?true:false=>login
+// num=30;
+// num=29?console.log("raman"):console.log("rohit")
+// function
+// most important is Arrow (=>) function
+// function  ioi(){
+    // console.log ("hello world");
+
+// }
+// ioi ()
