6dd072f5f07c9335c86f6fcf4e17a97d48239c55
Vijay6923 <kumarviju2468@gmail.com>
2024-05-24 11:18:05 +0530

    javascript

---
+++ HOF/script.js
@@ -0,0 +1,25 @@
+// setTimeout(()=>{
+//     console.log("hello")
+// },5000)
+// const num=[1,2,3,4,5]
+
+// const total=num.map((num)=>num*num)
+// console.log(total);
+// filter
+// const country=["finland","ireland","pakistan"]
+// const const1=country.filter((country)=>country.endsWith("land"));
+// console.log(const1) 
+// const names=["anurag","vijay","rohit","parv","nishchay"]
+// let[name1,name2, name3,name4,name5]=names
+// console.log(name1,name2,name3,name4,name5)
+// const rectangle={
+//     width: 4,
+//     height:4,
+//     area:16,
+// };
+// let{width,height,area}=rectangle
+// console.log(width,height,area)
+
+
+
+
