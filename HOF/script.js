662b7a64679ee163cafcd1216468bd16093080c4
Vijay6923 <kumarviju2468@gmail.com>
2024-05-28 22:54:19 +0530

    javascript

--- HOF/script.js
+++ HOF/script.js
@@ -19,6 +19,13 @@
 // };
 // let{width,height,area}=rectangle
 // console.log(width,height,area)
+// spread and rest
+const names=[1,2,3,4,5,6,7,8,9]
+let[num1,num2,...args]=names
+console.log(...args);
+
+
+
 
 
 
